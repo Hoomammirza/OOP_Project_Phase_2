903d8fb4f81b5cc4721038b94e2acc336f0ceb4b
lenovo <arshia13848484@gmail.com>
2024-07-09 00:15:58 +0430

    update

--- src/main/java/com/example/oop_project_phase2/Game/Gameinit.java
+++ src/main/java/com/example/oop_project_phase2/Game/Gameinit.java
@@ -13,57 +13,8 @@ public class Gameinit {
     public static boolean wager;
     public static int wagerint;
     public static User Host,Guest;
-    private static Scanner input = new Scanner(System.in);
 
     public  void initGuest() {
-        Host = Users.LoginUser;
-        boolean quit = false;
-        Matcher showcurrrentmenu;
-
-        System.out.println("commands:\n" +
-                "*  user login -u <username> -p <password>\n"
-                + "*  show current menu ");
-        //=================================================================================================================
-        System.out.println("Please select you character Player 1:\n" +
-                "Gunner , Fighter , Dancer , Wizard ");
-        Matcher selectCharacter;
-
-        System.out.println("commands:\n" +
-                "*  select <character>\n"
-                + "*  show current menu ");
-
-        while (!quit) {
-            String in = input.nextLine();
-            selectCharacter = Misc.getMatcher(in, "^select(\\s+)(?<character>\\S+)(\\s*)$");
-            showcurrrentmenu = Misc.getMatcher(in, "^show current menu(\\s*)$");
-            if (selectCharacter.find()) {
-                if (SelectCharacter(Host,selectCharacter))
-                    quit = true;
-            } else if (showcurrrentmenu.find()) {
-                System.out.println("Game menu: Character Select");
-            }
-        }
-        //=================================================================================================================
-        System.out.println("Please select you character Player 2:\n" +
-                "Gunner , Fighter , Dancer , Wizard ");
-        quit = false;
-
-        System.out.println("commands:\n" +
-                "*  select <character>\n"
-                + "*  show current menu ");
-
-        while (!quit) {
-            String in = input.nextLine();
-            selectCharacter = Misc.getMatcher(in, "^select(\\s+)(?<character>\\S+)(\\s*)$");
-            showcurrrentmenu = Misc.getMatcher(in, "^show current menu(\\s*)$");
-            if (selectCharacter.find()) {
-                if (SelectCharacter(Guest,selectCharacter))
-                    quit = true;
-            } else if (showcurrrentmenu.find()) {
-                System.out.println("Game menu: Character Select");
-            }
-        }
-
     }
     public static boolean SelectCharacter(User user,Matcher matcher){
         String character = matcher.group("character");
@@ -86,6 +37,8 @@ public class Gameinit {
         }
         return true;
     }
+    public void initialize()
+    {
+
+    }
 }
-//diasble
-//invisible
