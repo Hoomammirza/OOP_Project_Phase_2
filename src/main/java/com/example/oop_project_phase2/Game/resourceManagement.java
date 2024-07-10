41b835eca80aeb9507f7fa8ff21fc20f097cfd0c
lenovo <arshia13848484@gmail.com>
2024-07-10 17:18:34 +0430

    update

---
+++ src/main/java/com/example/oop_project_phase2/Game/resourceManagement.java
@@ -0,0 +1,21 @@
+package com.example.oop_project_phase2.Game;
+
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+import java.io.File;
+
+public class resourceManagement {
+    public static Image cell1;
+    public static ImageView cell;
+    static {
+        try {
+            Image cell1 = new Image(new File(""));
+            imagesinger1.setImage(image);
+        }
+        catch (Exception e)
+        {
+
+        }
+    }
+}
